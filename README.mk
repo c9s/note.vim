4ce44ca28aac049a561e661ca9ad853a715eac8f
c9s <cornelius.howl@gmail.com>
2010-06-03 02:47:04 +0800

    Update doc

--- README.mk
+++ README.mk
@@ -1,5 +1,11 @@
 
 
+Simple Note Vim Plugin
+======================
+
+simple note vim plugin let you create plain text files with an note id in your note
+file directory. the note id will be auto-generated.
+
 Command
 =======
 
